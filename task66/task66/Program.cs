1eddddd71116fd6aaddb6eb576443cf12292243f
Pavel Geraskin <pavel.geraskin@gmail.com>
2023-10-23 14:06:59 +0300

    Add tasks

---
+++ task66/task66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int FindSum(int nM, int nN)
+{
+  
+    // выход из рекурсии
+    if (nM >= nN)
+        return 0;
+    else
+    {
+        nM++;// увеличиваем на 1
+        return nM + FindSum(nM, nN);
+    }
+}
+
+int N = 0;
+int M = 0;
+Console.WriteLine("Программа ищет сумму натуральных элементов в промежутке от M до N\nВведите числа M и N:");
+if (int.TryParse(Console.ReadLine(), out M) && (int.TryParse(Console.ReadLine(), out N)) && M > 0 && N > 0)
+{
+    Console.WriteLine($"Сумма чисел от {M} до {N} = {FindSum(M - 1, N)}");
+}
+else
+{
+    Console.WriteLine("Вы не ввели натуральные числа N и M!");
+}
+
+
---
+++ task68/task68/Program.cs
@@ -0,0 +1,43 @@
+﻿// функция Аккермана
+// A(n, m) такая, что:
+// A(0,m) = m+1
+// A(n+1,0) = A(n,1)
+// A(n+1, m+1) = A(n,A(n+1,m))
+
+int Ack (int n, int m)
+{
+
+        // Поскольку для вычесления функции требуется очень большое число рекурсивных итераций, при n > 3 возможно переполнение буфера
+        if (n > 3)
+        {
+            Console.WriteLine("Вы ввели слишком большое n (>3)!");
+            return -1; // отрицательное значение сигнализирует об ошибке
+        }
+        while (n != 0)
+        {
+            if (m == 0)
+                m = 1;
+            else
+            {
+                m = Ack(n, m - 1);
+            }
+
+            n = n - 1;
+        Console.Write("*"); //Нарисуем "индикатор" вычислений
+        }
+
+        return m + 1;
+      
+}
+
+int nN = 0;
+int nM = 0;
+
+Console.WriteLine("Программа вычисляет значение функции Анкермана(N,M) для N <=3\nВведите N и M:");
+if (int.TryParse(Console.ReadLine(), out nN) && (int.TryParse(Console.ReadLine(), out nM)))
+{
+    Console.WriteLine($"\nAckermann({nN},{nM}) result = {Ack(nN, nM)}");
+}
+else
+    Console.WriteLine("Вы ввели неверные значения!");
+
